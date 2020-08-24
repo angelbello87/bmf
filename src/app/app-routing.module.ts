8fb07959c788c767f52aa0f233120365ba7efea9
Angel <angelbello87@yahoo.com>
2020-08-24 14:09:59 -0500

    add more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,14 @@ import { MembershipComponent } from './app-components/membership-component/membe
 
 
 const routes: Routes = [
+  { path:"", pathMatch: 'full', redirectTo:'home'},
   { path: "home", component:HomeComponent}, 
   { path:"membership", component:MembershipComponent},
   { path:"donations", component:DonationsComponent},
   { path:"sitecheckin", component:WaiverCheckinComponent},
   { path:"members", component:MembersComponent},
   { path:"links", component:LinksComponent},
-  {path: "**", component:HomeComponent},
+  { path: "**", pathMatch: 'full', redirectTo:'home'},
 ];
 
 @NgModule({
