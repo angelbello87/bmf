edc963cc82ed572e1e5bb547296bfc838e0bc83d
Angel <angelbello87@yahoo.com>
2020-08-25 10:08:57 -0500

    add more info

--- src/app/app-modules/material/material.module.ts
+++ src/app/app-modules/material/material.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+
 
 
 
@@ -9,9 +15,19 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     CommonModule,
     MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
   ],
   exports:[
     MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
   ],
 })
 export class MaterialModule { }
